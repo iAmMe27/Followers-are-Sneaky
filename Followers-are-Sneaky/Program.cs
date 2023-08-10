7318369f3ac2d0bce313cad5a2a4872bee08646d
iAmMe27 <iAmMe27@protonmail.com>
2023-08-10 17:51:51 +0100

    Update Program.cs

--- Followers-are-Sneaky/Program.cs
+++ Followers-are-Sneaky/Program.cs
@@ -1,6 +1,7 @@
 using Mutagen.Bethesda;
-using Mutagen.Bethesda.Synthesis;
+using Mutagen.Bethesda.Plugins;
 using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Synthesis;
 using Noggog;
 
 namespace FollowersAreSneaky
@@ -10,7 +11,13 @@ namespace FollowersAreSneaky
         public static async Task<int> Main(string[] args)
         {
             return await SynthesisPipeline.Instance
-                .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
+                .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch, new PatcherPreferences()
+                {
+                    ExclusionMods = new List<ModKey>()
+                    {
+                        new ModKey("FollowersAreSneaky.esp", ModType.Plugin)
+                    }
+                })
                 .SetTypicalOpen(GameRelease.SkyrimSE, "FollowersAreSneaky.esp")
                 .Run(args);
         }
@@ -20,7 +27,7 @@ namespace FollowersAreSneaky
             int count = 0;
 
             // For every NPC, check for the follower faction
-            foreach(var NPC in state.LoadOrder.PriorityOrder.WinningOverrides<INpcGetter>())
+            foreach (var NPC in state.LoadOrder.PriorityOrder.WinningOverrides<INpcGetter>())
             {
                 foreach (var Faction in NPC.Factions)
                 {
@@ -30,6 +37,13 @@ namespace FollowersAreSneaky
                         continue;
                     }
 
+                    // If the NPC has the flag set already, skip them
+                    if (NPC.Configuration.Flags.HasFlag(NpcConfiguration.Flag.DoesntAffectStealthMeter))
+                    {
+                        continue;
+                    }
+                
+                    System.Console.WriteLine("Patching " + NPC.Name);
                     state.PatchMod.Npcs.GetOrAddAsOverride(NPC).Configuration.Flags = NPC.Configuration.Flags.SetFlag(NpcConfiguration.Flag.DoesntAffectStealthMeter, true);
                     count++;
                 }
